343b5a6a8c2d25826acaa4403cb014a57b273ea3
Christian Thomas BADOLO <167626485+ChristianthomasBADOLO@users.noreply.github.com>
2024-09-30 09:27:46 +0100

    Create postgres_operations.py

---
+++ dags/postgres_operations.py
@@ -0,0 +1,14 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+def insert_book_data_into_postgres(ti):
+    book_data = ti.xcom_pull(key='book_data', task_ids='fetch_book_data')
+    if not book_data:
+        raise ValueError("No book data found")
+
+    postgres_hook = PostgresHook(postgres_conn_id='books_connection')
+    insert_query = """
+    INSERT INTO books (title, authors, price, rating)
+    VALUES (%s, %s, %s, %s)
+    """
+    for book in book_data:
+        postgres_hook.run(insert_query, parameters=(book['Title'], book['Author'], book['Price'], book['Rating']))
