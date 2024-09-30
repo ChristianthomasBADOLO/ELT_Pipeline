ba0209f58f85ccd9e7e5862b804db1eb812ad0bc
Christian Thomas BADOLO <167626485+ChristianthomasBADOLO@users.noreply.github.com>
2024-09-30 09:27:39 +0100

    Create config_file.py

---
+++ dags/config_file.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 6, 20),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+headers = {
+    "Referer": 'https://www.amazon.com/',
+    "Sec-Ch-Ua": "Not_A Brand",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "macOS",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+NUM_BOOKS_TO_FETCH = 50
+
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+#     'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+#     'Referer': 'https://www.google.com/',
+#     'DNT': '1',
+#     'Connection': 'keep-alive',
+#     'Upgrade-Insecure-Requests': '1',
+# }
