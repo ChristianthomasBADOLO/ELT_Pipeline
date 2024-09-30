684555b81af73c2c0dd3422e1e773d40dbb49eed
Christian Thomas BADOLO <167626485+ChristianthomasBADOLO@users.noreply.github.com>
2024-09-30 18:15:28 +0100

    Update config_file.py

--- dags/config_file.py
+++ dags/config_file.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 9, 30, 16, 30),
+    'start_date': datetime(2024, 9, 30, 16, 50),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
